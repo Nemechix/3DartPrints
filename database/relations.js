389ad950d0f2f1fb8a23e35662e41ede762bc4af
Crayyarc <crayyarc@gmail.com>
2023-03-27 16:53:11 +0100

    add dbsql, dbtxt mod relations

--- database/relations.js
+++ database/relations.js
@@ -5,36 +5,41 @@ const Category = require('../api/models/category.models')
 const Material = require('../api/models/material.models')
 const Order = require('../api/models/order.models')
 
+function addRelationsToModels() {
+    try {
+            //table Order_designs
+            User.hasMany(Design)
+            Design.belongsTo(User)
 
-//table Order_designs
-User.hasMany(Design)
-Design.belongsTo(User)
+            //table design_category
+            Category.hasMany(Design)
+            Design.belongsTo(Category)
 
-//table design_category
-Category.hasMany(Design)
-Design.belongsTo(Category)
-
-//table user_printer
-User.belongsToMany(Printer, { through: 'user_printer' })
-Printer.belongsToMany(User, { through: 'user_printer' })
-
-
-//table printer_materials
-Material.belongsToMany(User, Printer, { through: 'printer_materials' })
-User.belongsToMany(Printer, Material, { through: 'printer_materials' })
-Printer.belongsToMany(User, Material, { through: 'printer_materials' })
-
-//table order_prints
-Order.hasOne(User)
-Order.hasOne(Design)
-Order.hasOne(Printer)
-Order.hasOne(Material)
-User.belongsTo(Order)
-Design.belongsTo(Order)
-Printer.belongsTo(Order)
-Material.belongsToMany(Order)
+            //table user_printer
+            User.belongsToMany(Printer, { through: 'user_printer' })
+            Printer.belongsToMany(User, { through: 'user_printer' })
 
 
+            //table printer_materials
+            Material.belongsToMany(User, Printer, { through: 'printer_materials' })
+            User.belongsToMany(Printer, Material, { through: 'printer_materials' })
+            Printer.belongsToMany(User, Material, { through: 'printer_materials' })
+
+            //table order_prints
+            Order.hasOne(User)
+            Order.hasMany(Design)
+            Order.hasOne(Printer)
+            Order.hasOne(Material)
+            User.belongsTo(Order)
+            Design.belongsTo(Order)
+            Printer.belongsTo(Order)
+            Material.belongsToMany(Order)
+
+        console.log('Relations added to all models')
+    } catch (error) {
+        throw error
+    }
+}
 
 module.exports = Relations
 
