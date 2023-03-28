f5f9ab7a35705f8a97209ce77d41961c92f698cd
Nemechix <nemesi.jobs@gmail.com>
2023-03-28 11:23:01 +0100

    getDesignById route and controller

--- api/controllers/design.controller.js
+++ api/controllers/design.controller.js
@@ -54,4 +54,17 @@ async function deleteDesignById(req, res) {
     }
 }
 
-module.exports = { getAllDesign,createDesign, updateDesignById, deleteDesignById }
+async function getDesignById(req, res) {
+    try {
+        const design = await Design.findByPk(req.params.id)
+        if (design) {
+            return res.status(200).json(design)
+        } else {
+            return res.status(404).send('Design not found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+module.exports = { getAllDesign,createDesign, updateDesignById, deleteDesignById, getDesignById }
--- api/routes/design.routes.js
+++ api/routes/design.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 
 const {
-    getAllDesign, createDesign, updateDesignById, deleteDesignById
+    getAllDesign, createDesign, updateDesignById, deleteDesignById, getDesignById
 } = require('../controllers/design.controller')
 
 router.get('/', getAllDesign)
+router.get('/:designId', getDesignById)
 router.post('/', createDesign)
 router.put('/:designId', updateDesignById)
 router.delete('/:designId', deleteDesignById)
