95b1d81f9576f1a6ac43035ee1a6741954772ff1
Nemechix <nemesi.jobs@gmail.com>
2023-03-28 11:16:18 +0100

    minor changer

--- api/routes/design.routes.js
+++ api/routes/design.routes.js
@@ -3,8 +3,6 @@ const router = require('express').Router()
 const {
     getAllDesign, createDesign, updateDesignById, deleteDesignById
 } = require('../controllers/design.controller')
-const { getUserDesignsById } = require('../controllers/user.controller')
-
 
 router.get('/', getAllDesign)
 router.post('/', createDesign)
