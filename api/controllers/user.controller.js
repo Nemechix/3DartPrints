ecf8fc665e0be7840f8f1c700439d3a521f33b11
Nemechix <nemesi.jobs@gmail.com>
2023-03-28 10:01:31 +0100

    user routes  updated with controller changes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -48,10 +48,28 @@ async function deleteUserById(req,res){
     }
 }
 
+async function updateUserById(req, res) {
+    try {
+        const [updated] = await User.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        })
+        console.log(updated)
+        if (updated) {
+            return res.status(200).json({ message: 'User updated' })
+        } else {
+            return res.status(404).send('User not found')
+        }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
 /* async function getDesignByUserId(req,res) {
     try{
         const user = await User.findByPk
     }
 } */
 
-module.exports = {getAllUsers, createUser, getUserById, deleteUserById}
+module.exports = {getAllUsers, createUser, getUserById, deleteUserById, updateUserById}
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 
 const {
-    getAllUsers,
-    createUser
+    getAllUsers, createUser, getUserById, deleteUserById, updateUserById
 } = require('../controllers/user.controller')
 
 router.get('/', getAllUsers)
 router.post('/', createUser)
+router.get('/', getUserById)
+router.put('/', updateUserById)
+router.delete('/', deleteUserById)
 
 
 module.exports = router
