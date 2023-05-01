b94bef2231ed4d32ef6bd972773e4a7f15af6613
Nemechix <nemesi.jobs@gmail.com>
2023-05-01 15:27:32 +0000

    image fix

--- api/models/category.models.js
+++ api/models/category.models.js
@@ -10,7 +10,7 @@ const Category = sequelize.define(
         },
         image: {
             type: DataTypes.STRING,
-            allowNull: true,
+            allowNull: false,
         },
     },
     { updatedAt: false }
