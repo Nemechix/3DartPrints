1105f27dc7e365a8fade4db67bfc52aeb11f6a6d
Nemechix <nemesi.jobs@gmail.com>
2023-05-01 14:28:01 +0000

    fix

--- api/routes/category.routes.js
+++ api/routes/category.routes.js
@@ -9,13 +9,13 @@ const {
     deleteCategoryById,
    // getDesignsByCategoryId,
     getRandomCategory,
-    getDesignsByCategoryName
+    //getDesignsByCategoryName
 } = require('../controllers/category.controller')
 
 router.get('/', getAllCategories)
 router.get('/random', getRandomCategory)
 //router.get('/:id/designs', getDesignsByCategoryId)
-router.get('/:name/designs', getDesignsByCategoryName)
+//router.get('/:name/designs', getDesignsByCategoryName)
 router.get('/:id', getCategoryById)
 router.post('/', checkAuth, checkAdmin, createCategory)
 router.put('/:id', checkAuth, checkAdmin, updateCategoryById)
