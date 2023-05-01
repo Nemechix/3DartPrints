6c49484ede0a694506d64a16625f01565aa48649
Nemechix <nemesi.jobs@gmail.com>
2023-05-01 12:54:59 +0000

    categorys fix

--- api/controllers/category.controller.js
+++ api/controllers/category.controller.js
@@ -84,7 +84,7 @@ async function getDesignsByCategoryId(req, res) {
         if (desings.length) {
             return res.status(200).json(desings);
         } else {
-            return res.status(404).send('User not found');
+            return res.status(404).send('Category not found');
         }
     } catch (error) {
         res.status(500).send(error.message);
