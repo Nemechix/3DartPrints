0ae755bccd7568f1af6244a8d2f2c7577a52a2e7
Nemechix <nemesi.jobs@gmail.com>
2023-05-01 14:14:26 +0000

    fix

--- api/routes/category.routes.js
+++ api/routes/category.routes.js
@@ -15,7 +15,7 @@ const {
 router.get('/', getAllCategories)
 router.get('/random', getRandomCategory)
 router.get('/:id/designs', getDesignsByCategoryId)
-router.get('/:id/designs', getDesignsByCategoryName)
+router.get('/:name/designs', getDesignsByCategoryName)
 router.get('/:id', getCategoryById)
 router.post('/', checkAuth, checkAdmin, createCategory)
 router.put('/:id', checkAuth, checkAdmin, updateCategoryById)
