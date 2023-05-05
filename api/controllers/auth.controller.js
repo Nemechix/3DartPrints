058a66d0d304fb402e46c078cbc613b95b3253c7
DaniSuarezG <danielsuarezgalvez@gmail.com>
2023-05-06 00:11:40 +0100

    Just Trying Deploy

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const signUp = async (req, res) => {
     try {
     req.body.password = bcrypt.hashSync(req.body.password, 10)
-    req.body.role = 'user'
+    // req.body.role = 'user'
     const user = await User.create(req.body)    
     res.status(200).json(user)
     } catch (error) {
