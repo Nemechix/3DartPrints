a28fd33cf2814f9abc12134381951efd1b3a16a3
Nemechix <nemesi.jobs@gmail.com>
2023-05-08 12:32:55 +0100

    stripe defined

--- api/routes/stripe.routes.js
+++ api/routes/stripe.routes.js
@@ -1,7 +1,9 @@
 // price_1N5RP8Lq0Q7kbXgYpEsyfyyI cafe
 //price_1N5RQzLq0Q7kbXgYT3YR62cr gafas
 //price_1N5RS6Lq0Q7kbXgYuYQnWyZr camera
-//sk_test_51N4PBRLq0Q7kbXgYAxRg1hiePFEE5E5QVivcLfo1S3qIfz6prhLArOpy1iaF9ZkZqU2rTqErJjm9GcGwWgWhmpck00nvP7r8Ak
+
+
+const stripe = require('stripe')('sk_test_51N4PBRLq0Q7kbXgYAxRg1hiePFEE5E5QVivcLfo1S3qIfz6prhLArOpy1iaF9ZkZqU2rTqErJjm9GcGwWgWhmpck00nvP7r8Ak')
 
 
 const router = require('express').Router()
